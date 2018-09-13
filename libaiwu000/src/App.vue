543cddc9ea0e03bdc49a26322d255651aadf54ac
lanouhn <347251606@qq.com>
2018-09-13 19:45:07 +0800

    hh

--- libaiwu000/src/App.vue
+++ libaiwu000/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h01gylbw></h01gylbw>
+
+
+
     <router-view/>
   </div>
 </template>
 
 <script>
+  import  H01gylbw from '@/components/H01gylbw'
+
+
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    h01gylbw:H01gylbw,
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>

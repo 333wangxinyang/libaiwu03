9dae44c0436a118abc95f51d87b069989d10d3df
lanouhn <347251606@qq.com>
2018-09-13 20:10:52 +0800

    no message

--- libaiwu000/src/App.vue
+++ libaiwu000/src/App.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="app">
-
     <h01gylbw></h01gylbw>
-
-
-
-
-<!--<x01></x01>-->
-    <x02></x02>
-
-    <head01></head01>
-
-
+    <!--<x01></x01>-->
+    <!--<x02></x02>-->
+    <!--<head01></head01>-->
     <router-view/>
   </div>
 </template>
 
 <script>
 
-  import  H01gylbw from '@/components/H01gylbw'
+
   import X01 from '@/components/wxy/X01'
   import X02 from '@/components/wxy/X02'
   import head01 from '@/components/wangxinyang/head01'
+  import H01gylbw from '@/components/H01gylbw'
 export default {
   name: 'App',
   components:{
@@ -30,15 +23,8 @@ export default {
     x01:X01,
     x02:X02
   },
-
-
->>>>>>> d4af32f999d35e2e475c64af5989fc69f442e712
 }
 </script>
 
 <style>
-<<<<<<< HEAD
-=======
-
->>>>>>> d4af32f999d35e2e475c64af5989fc69f442e712
 </style>
---
+++ libaiwu000/src/components/HZH/H01gylbw.vue
@@ -0,0 +1,240 @@
+<template>
+  <div class="content">
+    <!--导航栏-->
+    <div class="nav">
+      <div class="nav1">
+        <p>个人中心</p>
+      </div>
+      <div class="nav2">
+        <ul>
+          <li class="max"><a href="###">交易管理<span>></span></a></li>
+          <li><router-link to="/wodezhanghu">我的账户</router-link></li>
+          <li><router-link to="/wodedingdan">我的订单</router-link></li>
+          <li><a href="###">我的积分</a></li>
+          <li><a href="###">积分订单</a></li>
+          <li><a href="###">我的钱包</a></li>
+          <li class="max"><a href="###">会员资料<span>></span></a></li>
+          <li><router-link to="/gerenziliao">个人资料</router-link></li>
+          <li><router-link to="/dizhiguanli">地址管理</router-link></li>
+          <li><a href="###">我的收藏</a></li>
+          <li><a href="###">最近浏览</a></li>
+          <li><a href="###">修改密码</a></li>
+          <li class="max"><a href="###">站内信<span>></span></a></li>
+          <li><a href="###">我的消息</a></li>
+          <li><a href="###">意见反馈</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="body">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Gerenzhongxin",
+    data(){
+      return {
+
+      }
+    },
+    mounted(){
+      $('.nav2 ul li a').click(function () {
+        console.log($('.nav2 ul li a')[0])
+        for (var i = 0;i < $('.nav2 ul li a').length;i++){
+          if(this!=$('.nav2 ul li a')[0] || this!=$('.nav2 ul li a')[6] || this!=$('.nav2 ul li a')[12]){
+            $('.nav2 ul li a')[0].style.color = '#666666';
+            $('.nav2 ul li a')[6].style.color = '#666666';
+            $('.nav2 ul li a')[12].style.color = '#666666';
+          }
+          $('.nav2 ul li a')[i].style.borderBottom = '0';
+          $('.nav2 ul li a')[i].style.color = '#999999';
+        }
+        this.style.borderBottom = '#63a365 solid 1px';
+        this.style.color = '#63a365'
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .content{
+    margin-top: 40px;
+    width: 1280px;
+  }
+  /*导航栏*/
+  .nav{
+    width: 170px;
+    height: 625px;
+    float: left;
+    border: solid #ececec 1px;
+  }
+  .body{
+    width: 1087px;
+    /*height: 625px;*/
+    float: right;
+    border: solid #ececec 1px;
+  }
+  .nav1{
+    height: 57px;
+    border-bottom: 1px solid #ececec;
+  }
+  .nav1>p{
+    line-height: 57px;
+    margin-left: 20px;
+    font-size: 17px;
+    color: #666666;
+  }
+  .nav2{
+    height: 567px;
+    overflow: hidden;
+  }
+  .nav2>ul{
+    margin: 20px 0;
+    height: 527px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .nav2>ul>li>a{
+    display: inline;
+    margin-left: 20px;
+    font-size: 15px;
+    color: #999999;
+  }
+  .nav2>ul>li{
+    display: inline;
+  }
+  .nav2>ul>li>a>span{
+    float: right;
+    margin-right: 48px;
+  }
+  .nav2>ul>.max>a{
+    font-size: 18px;
+    color:#666666;
+  }
+  .nav2>ul>.max>a>span{
+    font-size: 16px;
+  }
+</style>
+<template>
+  <div class="content">
+    <!--导航栏-->
+    <div class="nav">
+      <div class="nav1">
+        <p>个人中心</p>
+      </div>
+      <div class="nav2">
+        <ul>
+          <li class="max"><a href="###">交易管理<span>></span></a></li>
+          <li><router-link to="/wodezhanghu">我的账户</router-link></li>
+          <li><router-link to="/wodedingdan">我的订单</router-link></li>
+          <li><a href="###">我的积分</a></li>
+          <li><a href="###">积分订单</a></li>
+          <li><a href="###">我的钱包</a></li>
+          <li class="max"><a href="###">会员资料<span>></span></a></li>
+          <li><router-link to="/gerenziliao">个人资料</router-link></li>
+          <li><router-link to="/dizhiguanli">地址管理</router-link></li>
+          <li><a href="###">我的收藏</a></li>
+          <li><a href="###">最近浏览</a></li>
+          <li><a href="###">修改密码</a></li>
+          <li class="max"><a href="###">站内信<span>></span></a></li>
+          <li><a href="###">我的消息</a></li>
+          <li><a href="###">意见反馈</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="body">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Gerenzhongxin",
+    data(){
+      return {
+
+      }
+    },
+    mounted(){
+      $('.nav2 ul li a').click(function () {
+        console.log($('.nav2 ul li a')[0])
+        for (var i = 0;i < $('.nav2 ul li a').length;i++){
+          if(this!=$('.nav2 ul li a')[0] || this!=$('.nav2 ul li a')[6] || this!=$('.nav2 ul li a')[12]){
+            $('.nav2 ul li a')[0].style.color = '#666666';
+            $('.nav2 ul li a')[6].style.color = '#666666';
+            $('.nav2 ul li a')[12].style.color = '#666666';
+          }
+          $('.nav2 ul li a')[i].style.borderBottom = '0';
+          $('.nav2 ul li a')[i].style.color = '#999999';
+        }
+        this.style.borderBottom = '#63a365 solid 1px';
+        this.style.color = '#63a365'
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .content{
+    margin-top: 40px;
+    width: 1280px;
+  }
+  /*导航栏*/
+  .nav{
+    width: 170px;
+    height: 625px;
+    float: left;
+    border: solid #ececec 1px;
+  }
+  .body{
+    width: 1087px;
+    /*height: 625px;*/
+    float: right;
+    border: solid #ececec 1px;
+  }
+  .nav1{
+    height: 57px;
+    border-bottom: 1px solid #ececec;
+  }
+  .nav1>p{
+    line-height: 57px;
+    margin-left: 20px;
+    font-size: 17px;
+    color: #666666;
+  }
+  .nav2{
+    height: 567px;
+    overflow: hidden;
+  }
+  .nav2>ul{
+    margin: 20px 0;
+    height: 527px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .nav2>ul>li>a{
+    display: inline;
+    margin-left: 20px;
+    font-size: 15px;
+    color: #999999;
+  }
+  .nav2>ul>li{
+    display: inline;
+  }
+  .nav2>ul>li>a>span{
+    float: right;
+    margin-right: 48px;
+  }
+  .nav2>ul>.max>a{
+    font-size: 18px;
+    color:#666666;
+  }
+  .nav2>ul>.max>a>span{
+    font-size: 16px;
+  }
+</style>

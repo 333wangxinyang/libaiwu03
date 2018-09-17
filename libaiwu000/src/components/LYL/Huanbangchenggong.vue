3928791e1adda7c4171b4f2b7b9b14f5074752cd
lanouhn <lanouhn@liyongliang.local>
2018-09-17 11:13:21 +0800

    lyl

---
+++ libaiwu000/src/components/LYL/Huanbangchenggong.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      换绑成功
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Huanbangchenggong"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ libaiwu000/src/components/LYL/Xinshouji.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      新手机
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Xinshouji"
+    }
+</script>
+
+<style scoped>
+
+</style>

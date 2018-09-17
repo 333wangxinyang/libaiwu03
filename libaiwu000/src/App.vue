c7d5b32dec161af129d002aacbfe6afcb0f21e88
lanouhn <347251606@qq.com>
2018-09-17 09:05:14 +0800

    1111

--- libaiwu000/src/App.vue
+++ libaiwu000/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
-    <h01gylbw></h01gylbw>
-
+    <!--<h01gylbw></h01gylbw>-->
+    <!--<h02drzc></h02drzc>-->
     <!--<x01></x01>-->
     <!--<x02></x02>-->
     <!--<head01></head01>-->
-
+    <!--<h03dr></h03dr>-->
+    <h04wjmm></h04wjmm>
 
 
 
@@ -28,17 +29,17 @@
   import X01 from '@/components/wxy/X01'
   import X02 from '@/components/wxy/X02'
   import head01 from '@/components/wangxinyang/head01'
-
+  import H02drzc from '@/components/HZH/H02drzc'
 
   import H01gylbw from '@/components/HZH/H01gylbw'
 
+
+
+
   import seek from '@/components/wangxinyang/seek'
   import navigation from '@/components/wangxinyang/navigation'
-
-
-  import seek from '@/components/wangxinyang/seek'
-  import navigation from '@/components/wangxinyang/navigation'
-
+  import H03dr from '@/components/HZH/H03dr'
+  import H04wjmm from '@/components/HZH/H04wjmm'
 export default {
   name: 'App',
   components:{
@@ -47,6 +48,10 @@ export default {
     x02:X02,
     seek:seek,
     navigation:navigation,
+    h01gylbw:H01gylbw,
+    h02drzc:H02drzc,
+    h03dr:H03dr,
+    h04wjmm:H04wjmm
   },
 
 
